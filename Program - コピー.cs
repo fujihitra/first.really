26419eb7a97646ba3214129e45c31221f825ca87
fujihitra <Koinegai.rami.54@gmail.com>
2017-07-13 19:32:44 +0900

    wawawa

---
+++ Program - コピー.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleScript
+{
+    delegate void MyDelegate(int a, int b);
+    class ev
+    {
+        public event MyDelegate n;
+        public void OnSpace(int a,int b)
+        {
+            n(a, b);
+        }
+    }
+    class Increment
+    {
+        public void Add(int a, int b)
+        {
+            int x = a + b;
+            Console.WriteLine("デリゲートに格納したAdd関数の出力は" + x);
+        }
+    }
+
+    class OutPut
+    {
+        public static void Main()
+        {
+            string ch;
+            ch = Console.ReadLine();
+            Console.WriteLine(ch);
+            if (ch == " ") {
+                Console.WriteLine(ch);
+                ev e = new ev();
+            Increment tasu = new Increment();
+            e.n += new MyDelegate(tasu.Add);
+            e.OnSpace(5, 3);
+            MyDelegate mydelegate = new MyDelegate(tasu.Add);
+         }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleScript
+{
+
+    delegate int MyDelegate(int a, int b);
+
+    class Increment
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+
+    class OutPut
+    {
+        public static void Main()
+        {
+            Increment tasu = new Increment();
+            Console.WriteLine("そのまま呼び出しのAdd関数の出力は"+tasu.Add(5,3));
+            MyDelegate mydelegate =(a,b)=> tasu.Add(a,b);
+            Console.WriteLine("デリゲートに格納したAdd関数の出力は"+mydelegate(5,3));
+        }
+    }
+}
